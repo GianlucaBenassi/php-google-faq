df6b23e478531ee17f81b1a046a05868ad7cf7a6
Gianluca Benassi <benox1199@gmail.com>
2022-01-19 16:13:26 +0100

    dynamic load faqs list

--- index.php
+++ index.php
@@ -31,20 +31,20 @@
             "answer" => "
                 <p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p>
 
-                <ul>
+                <ol>
 
                     <li>
                         La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-                        <ul>
+                        <ol class='inside_list'>
                             <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
                             <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
-                        </ul>
+                        </ol>
 
                     </li>
 
                     <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
 
-                </ul>
+                </ol>
 
                 <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>
 
@@ -107,7 +107,14 @@
     </header>
 
     <main>
-
+        <div class="container">
+            <?php foreach ($faqs as $faq) { ?>
+                <div class="answare">
+                    <h2><?php echo $faq["question"]?></h2>
+                    <?php echo $faq["answer"] ?>
+                </div>
+            <?php }?>
+        </div>
     </main>
 
 </body>
